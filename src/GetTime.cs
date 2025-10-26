3a1f2aff7fa72f011890297e0785d5763297eb38
Sundrun <not@an.email>
2025-10-26 10:33:13 +0100

    r

--- src/GetTime.cs
+++ src/GetTime.cs
@@ -2,22 +2,17 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 
-public class HttpSqlFunction
+namespace MomentApi;
+
+public class HttpSqlFunction(SqlService sqlService)
 {
-    private readonly SqlService _sqlService;
-
-    public HttpSqlFunction(SqlService sqlService)
-    {
-        _sqlService = sqlService;
-    }
-
     [Function(nameof(GetTime))]
     public async Task<HttpResponseData> GetTime([HttpTrigger(AuthorizationLevel.Function, "get", Route = "time")] HttpRequestData req)
     {
         var response = req.CreateResponse();
         try
         {
-            var time = await _sqlService.GetServerTimeAsync();
+            var time = await sqlService.GetServerTimeAsync();
             await response.WriteStringAsync($"DB Server Time: {time}");
             response.StatusCode = HttpStatusCode.OK;
         }
