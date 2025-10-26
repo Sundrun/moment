a710f3ad7163a1b06498c284ed3e406cd5355a54
Sundrun <not@an.email>
2025-10-26 11:38:55 +0100

    context

---
+++ src/Infrastructure.Database/MomentContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database;
+
+public class MomentContext : DbContext
+{
+    DbSet<CoreMoment> Moments { get; set; }
+    DbSet<MomentOwner> MomentOwners { get; set; }
+    DbSet<MomentOwnership> MomentOwnerships { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("SqlConnectionString");
+        if (!string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("SqlConnectionString is not configured.");
+        }
+        
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
