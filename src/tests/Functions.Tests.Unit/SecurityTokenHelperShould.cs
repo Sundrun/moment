2a48ccded19c427bd1b10e0a21b41e09d22917c2
Sundrun <not@an.email>
2025-10-31 22:47:14 +0100

    IndicateFailureWhenNoHeadersArePresent

---
+++ src/tests/Functions.Tests.Unit/SecurityTokenHelperShould.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeAssertions;
+using Functions.Helpers;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using NSubstitute;
+
+namespace Functions.Tests.Unit;
+
+public class SecurityTokenHelperShould
+{
+    [Fact]
+    public void IndicateFailureWhenNoHeadersArePresent()
+    {
+        // Arrange
+        var context = Substitute.For<FunctionContext>();
+        var httpRequest = Substitute.For<HttpRequestData>(context);
+        httpRequest.Headers.Returns([]);
+        
+        // Act
+        var result = httpRequest.TryExtractToken(out _);
+        
+        // Assert
+        result.Should().BeFalse();
+    }
+}
