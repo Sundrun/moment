ada91ed1780ea6620ed078285fe3414858f77e05
Sundrun <not@an.email>
2025-12-01 21:34:10 +0100

    RetrieveExpectedMetadataTimestampDateTime

--- src/tests/Infrastructure.Database.Tests/MomentContextShould.cs
+++ src/tests/Infrastructure.Database.Tests/MomentContextShould.cs
@@ -223,4 +223,24 @@ public class MomentContextShould : IAsyncLifetime
         // Assert
         result.Should().Be(expected);
     }
+    
+    [Fact]
+    public async Task RetrieveExpectedMetadataTimestampDateTime()
+    {
+        // Arrange
+        var testStartTime = DateTimeOffset.UtcNow;
+        
+        var timestamp = new MetadataTimestamp();
+        await _dbContext.MetadataTimestamps.AddAsync(timestamp);
+        await _dbContext.SaveChangesAsync();
+        
+        // Act
+        var storedTimestamp = await _dbContext.MetadataTimestamps.FirstAsync();
+        var result = storedTimestamp.Timestamp.Timestamp;
+        
+        
+        // Assert
+        result.Should().BeAfter(testStartTime);
+        result.Should().BeBefore(DateTimeOffset.MaxValue);
+    }
 }
