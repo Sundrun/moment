6489e96f3e8c037162011c4e503af37fbe3d10d2
Sundrun <not@an.email>
2025-12-02 20:08:20 +0100

    GoogleIdentitiesShould

--- src/tests/Infrastructure.Database.Tests/MomentContextShould.cs
+++ src/tests/Infrastructure.Database.Tests/MomentContextShould.cs
@@ -40,33 +40,7 @@ public class MomentContextShould : IAsyncLifetime
     
     
     
-    [Fact]
-    public async Task StoreExpectedGoogleIdentity()
-    {
-        // Arrange
-        _dbContext.MomentOwners.Add(new MomentOwner());
-        await _dbContext.SaveChangesAsync();
-        var storedOwner = await _dbContext.MomentOwners.FirstOrDefaultAsync();
-        
-        var testSubject = new GoogleIdentitySubject("test-subject");
-        var newGoogleIdentity = new GoogleIdentity { Subject = testSubject };
-        var newGoogleIdentityOwner = new GoogleIdentityOwner
-        {
-            Owner = storedOwner!,
-            GoogleIdentity = newGoogleIdentity
-        };
-        await _dbContext.GoogleIdentities.AddAsync(newGoogleIdentity);
-        await _dbContext.GoogleIdentityOwners.AddAsync(newGoogleIdentityOwner);
-        await _dbContext.SaveChangesAsync();
-        
-        var expected = new GoogleIdentity { Id = new GoogleIdentityId(1), Subject = testSubject };
-        
-        // Act
-        var result = await _dbContext.GoogleIdentities.FirstOrDefaultAsync();
-        
-        // Assert
-        result.Should().BeEquivalentTo(expected);
-    }
+    
     
     [Fact]
     public async Task NotAllowDuplicateGoogleIdentities()
