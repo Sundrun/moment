4f37ea0740747834c1418e7ca212242d9cf4d76e
Sundrun <not@an.email>
2025-12-02 20:13:28 +0100

    MetadataTimestampsShould

--- src/tests/Infrastructure.Database.Tests/MomentContextTests/GoogleIdentitiesShould.cs
+++ src/tests/Infrastructure.Database.Tests/MomentContextTests/GoogleIdentitiesShould.cs
@@ -34,4 +34,36 @@ public class GoogleIdentitiesShould : MomentContextBase
         // Assert
         result.Should().BeEquivalentTo(expected);
     }
+    
+    [Fact]
+    public async Task NotAllowDuplicateGoogleIdentities()
+    {
+        // Arrange
+        _dbContext.MomentOwners.Add(new MomentOwner());
+        _dbContext.MomentOwners.Add(new MomentOwner());
+        await _dbContext.SaveChangesAsync();
+        
+        var ownerOne = await _dbContext.MomentOwners.FirstAsync(x => x.Id == new MomentOwnerId(1));
+        var ownerTwo = await _dbContext.MomentOwners.FirstAsync(x => x.Id == new MomentOwnerId(2));
+        
+        var testSubject = new GoogleIdentitySubject("test-subject");
+        var googleIdentity = new GoogleIdentity { Subject = testSubject };
+        var googleIdentityOwner = new GoogleIdentityOwner { Owner = ownerOne, GoogleIdentity = googleIdentity };
+
+        await _dbContext.GoogleIdentities.AddAsync(googleIdentity);
+        await _dbContext.GoogleIdentityOwners.AddAsync(googleIdentityOwner);
+        await _dbContext.SaveChangesAsync();
+        
+        var duplicateGoogleIdentity = new GoogleIdentity { Subject = testSubject };
+        var duplicateGoogleIdentityOwner = new GoogleIdentityOwner { Owner = ownerTwo, GoogleIdentity = duplicateGoogleIdentity};
+        
+        await _dbContext.GoogleIdentities.AddAsync(duplicateGoogleIdentity);
+        await _dbContext.GoogleIdentityOwners.AddAsync(duplicateGoogleIdentityOwner);
+        
+        // Act
+        var dbSaveTask = async () => await _dbContext.SaveChangesAsync();
+        
+        // Assert
+        await dbSaveTask.Should().ThrowAsync<DbUpdateException>();
+    }
 }
---
+++ src/tests/Infrastructure.Database.Tests/MomentContextTests/MetadataTimestampsShould.cs
@@ -0,0 +1,48 @@
+using AwesomeAssertions;
+using Entities;
+using Entities.Wrappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database.Tests.MomentContextTests;
+
+public class MetadataTimestampsShould : MomentContextBase
+{
+    [Fact]
+    public async Task RetrieveExpectedMetadataTimestampId()
+    {
+        // Arrange
+        var timestamp = new MetadataTimestamp();
+        await _dbContext.MetadataTimestamps.AddAsync(timestamp);
+        await _dbContext.SaveChangesAsync();
+
+        const long expected = 1;
+        
+        // Act
+        var storedTimestamp = await _dbContext.MetadataTimestamps.FirstAsync();
+        var result = storedTimestamp.Id.Id;
+        
+        
+        // Assert
+        result.Should().Be(expected);
+    }
+    
+    [Fact]
+    public async Task RetrieveExpectedMetadataTimestampDateTime()
+    {
+        // Arrange
+        var testStartTime = DateTimeOffset.UtcNow;
+        
+        var timestamp = new MetadataTimestamp();
+        await _dbContext.MetadataTimestamps.AddAsync(timestamp);
+        await _dbContext.SaveChangesAsync();
+        
+        // Act
+        var storedTimestamp = await _dbContext.MetadataTimestamps.FirstAsync();
+        var result = storedTimestamp.Timestamp.Timestamp;
+        
+        
+        // Assert
+        result.Should().BeAfter(testStartTime);
+        result.Should().BeBefore(DateTimeOffset.MaxValue);
+    }
+}
