0927d9e41d0349100d10b574007679a8d9fddf20
Sundrun <not@an.email>
2025-12-02 20:33:26 +0100

    RetrieveMomentTimestampsWithExpectedTimestamp

---
+++ src/tests/Infrastructure.Database.Tests/MomentContextTests/MomentTimestampsShould.cs
@@ -0,0 +1,37 @@
+using AwesomeAssertions;
+using Entities;
+using Entities.Wrappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database.Tests.MomentContextTests;
+
+public class MomentTimestampsShould : MomentContextBase
+{
+    [Fact]
+    public async Task RetrieveMomentTimestampsWithExpectedTimestamp()
+    {
+        // Arrange
+        var metadataTimestamp = new MetadataTimestamp();
+        await _dbContext.MetadataTimestamps.AddAsync(metadataTimestamp);
+        
+        var moment = new CoreMoment { Timestamp = new CoreMomentTimestamp(DateTimeOffset.UtcNow) };
+        await _dbContext.CoreMoments.AddAsync(moment);
+        
+        var momentTimestamp = new MomentTimestamp{ Moment = moment, Timestamp = metadataTimestamp };
+        await _dbContext.MomentTimestamps.AddAsync(momentTimestamp);
+        
+        await _dbContext.SaveChangesAsync();
+        
+        var expected = await _dbContext.MetadataTimestamps.FirstAsync();
+        
+        // Act
+        var storedMomentTimestamp = await _dbContext.MomentTimestamps
+            .Include(x => x.Moment)
+            .Include(x => x.Timestamp)
+            .FirstAsync();
+        var result = storedMomentTimestamp.Timestamp;
+        
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
